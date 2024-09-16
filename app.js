0b3629b263d570a7cecff7b0ef1904f1874bf023
AhmadAbdAlRhman <112072240+AhmadAbdAlRhman@users.noreply.github.com>
2024-09-16 18:38:44 +0300

    2th

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+console.log("Ahmad");
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
