fdb90d4818ad227715ee245ce841e4e6c39975af
AhmadAbdAlRhman <112072240+AhmadAbdAlRhman@users.noreply.github.com>
2024-10-20 16:13:51 +0300

    finish products

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongodbConnect = require('./util/database').mongodbConnect;
-
+const User = require("./models/user");
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,14 +16,13 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+app.use((req, _res, next) => {
+  User.fetchById("66f93a3b9c23ff5cf0836a65")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
--- models/user.js
+++ models/user.js
@@ -1,16 +1,34 @@
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
-
+const getDB = require("../util/database").getDB;
+const mongodb = require("mongodb");
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+  save() {
+    const db = getDB();
+    return db
+      .collection("User")
+      .insertOne(this)
+      .then((User) => {
+        console.log(User);
+        return User;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  static fetchById = (UserId) => {
+    const db = getDB();
+    return db
+      .collection("User")
+      .findOne({ _id: new mongodb.ObjectId(UserId) })
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
 module.exports = User;
